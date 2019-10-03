6e106a109e51226e578b880e2dc3de7db0998e16
monirulislam14 <49609116+monirulislam14@users.noreply.github.com>
2019-10-04 02:53:46 +0600

    Fire Range Automation System is a windows Application
    using ListView, List, TextBox, search Button , Radio Button etc

---
+++ FireRangeAutomation System/FireRangeAutomation System/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace FireRangeAutomation_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.solderNoTextBox = new System.Windows.Forms.TextBox();
+            this.solderNameTextBox = new System.Windows.Forms.TextBox();
+            this.scoreOneTextBox = new System.Windows.Forms.TextBox();
+            this.scoreTwoTextBox = new System.Windows.Forms.TextBox();
+            this.scoreThreeTextBox = new System.Windows.Forms.TextBox();
+            this.scoreFourTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.solderNoRadioButton = new System.Windows.Forms.RadioButton();
+            this.solderNameRadioButton = new System.Windows.Forms.RadioButton();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.infoListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.topAveargeScorerNameTextBox = new System.Windows.Forms.TextBox();
+            this.topTotalScorerTextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soldier Score Register";
+            // 
+            // solderNoTextBox
+            // 
+            this.solderNoTextBox.Location = new System.Drawing.Point(127, 63);
+            this.solderNoTextBox.Name = "solderNoTextBox";
+            this.solderNoTextBox.Size = new System.Drawing.Size(185, 20);
+            this.solderNoTextBox.TabIndex = 1;
+            // 
+            // solderNameTextBox
+            // 
+            this.solderNameTextBox.Location = new System.Drawing.Point(127, 89);
+            this.solderNameTextBox.Name = "solderNameTextBox";
+            this.solderNameTextBox.Size = new System.Drawing.Size(185, 20);
+            this.solderNameTextBox.TabIndex = 2;
+            // 
+            // scoreOneTextBox
+            // 
+            this.scoreOneTextBox.Location = new System.Drawing.Point(127, 116);
+            this.scoreOneTextBox.Name = "scoreOneTextBox";
+            this.scoreOneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreOneTextBox.TabIndex = 3;
+            // 
+            // scoreTwoTextBox
+            // 
+            this.scoreTwoTextBox.Location = new System.Drawing.Point(127, 143);
+            this.scoreTwoTextBox.Name = "scoreTwoTextBox";
+            this.scoreTwoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreTwoTextBox.TabIndex = 4;
+            // 
+            // scoreThreeTextBox
+            // 
+            this.scoreThreeTextBox.Location = new System.Drawing.Point(127, 170);
+            this.scoreThreeTextBox.Name = "scoreThreeTextBox";
+            this.scoreThreeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreThreeTextBox.TabIndex = 5;
+            // 
+            // scoreFourTextBox
+            // 
+            this.scoreFourTextBox.Location = new System.Drawing.Point(127, 197);
+            this.scoreFourTextBox.Name = "scoreFourTextBox";
+            this.scoreFourTextBox.Size = new System.Drawing.Size(100, 20);
+            this.scoreFourTextBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Soldier No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Soldier Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Target 1  Score";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Target 2  Score";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Target 3  Score";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Target 4  Score";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(237, 232);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 13;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(422, 20);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Soldier Information";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(422, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Search Criteria";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(425, 69);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Search By";
+            // 
+            // solderNoRadioButton
+            // 
+            this.solderNoRadioButton.AutoSize = true;
+            this.solderNoRadioButton.Location = new System.Drawing.Point(501, 69);
+            this.solderNoRadioButton.Name = "solderNoRadioButton";
+            this.solderNoRadioButton.Size = new System.Drawing.Size(74, 17);
+            this.solderNoRadioButton.TabIndex = 17;
+            this.solderNoRadioButton.TabStop = true;
+            this.solderNoRadioButton.Text = "Soldier No";
+            this.solderNoRadioButton.UseVisualStyleBackColor = true;
+            this.solderNoRadioButton.CheckedChanged += new System.EventHandler(this.solderNoRadioButton1_CheckedChanged);
+            // 
+            // solderNameRadioButton
+            // 
+            this.solderNameRadioButton.AutoSize = true;
+            this.solderNameRadioButton.Location = new System.Drawing.Point(603, 69);
+            this.solderNameRadioButton.Name = "solderNameRadioButton";
+            this.solderNameRadioButton.Size = new System.Drawing.Size(88, 17);
+            this.solderNameRadioButton.TabIndex = 18;
+            this.solderNameRadioButton.TabStop = true;
+            this.solderNameRadioButton.Text = "Soldier Name";
+            this.solderNameRadioButton.UseVisualStyleBackColor = true;
+            this.solderNameRadioButton.CheckedChanged += new System.EventHandler(this.solderNameRadioButton_CheckedChanged);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(519, 103);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(176, 20);
+            this.searchTextBox.TabIndex = 19;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(701, 103);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 20;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(701, 133);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 21;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // infoListView
+            // 
+            this.infoListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.infoListView.GridLines = true;
+            this.infoListView.Location = new System.Drawing.Point(428, 162);
+            this.infoListView.Name = "infoListView";
+            this.infoListView.Size = new System.Drawing.Size(348, 125);
+            this.infoListView.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.infoListView.TabIndex = 22;
+            this.infoListView.UseCompatibleStateImageBehavior = false;
+            this.infoListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Soldier No";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Soldier Name";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Average Score";
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total Score  ";
+            this.columnHeader4.Width = 80;
+            // 
+            // topAveargeScorerNameTextBox
+            // 
+            this.topAveargeScorerNameTextBox.Location = new System.Drawing.Point(588, 293);
+            this.topAveargeScorerNameTextBox.Name = "topAveargeScorerNameTextBox";
+            this.topAveargeScorerNameTextBox.Size = new System.Drawing.Size(165, 20);
+            this.topAveargeScorerNameTextBox.TabIndex = 23;
+            // 
+            // topTotalScorerTextBox
+            // 
+            this.topTotalScorerTextBox.Location = new System.Drawing.Point(588, 319);
+            this.topTotalScorerTextBox.Name = "topTotalScorerTextBox";
+            this.topTotalScorerTextBox.Size = new System.Drawing.Size(165, 20);
+            this.topTotalScorerTextBox.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(455, 300);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(112, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Top Average Scorerer";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(455, 326);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(96, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Top Total Scorerer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 346);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.topTotalScorerTextBox);
+            this.Controls.Add(this.topAveargeScorerNameTextBox);
+            this.Controls.Add(this.infoListView);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.solderNameRadioButton);
+            this.Controls.Add(this.solderNoRadioButton);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scoreFourTextBox);
+            this.Controls.Add(this.scoreThreeTextBox);
+            this.Controls.Add(this.scoreTwoTextBox);
+            this.Controls.Add(this.scoreOneTextBox);
+            this.Controls.Add(this.solderNameTextBox);
+            this.Controls.Add(this.solderNoTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fire Range Automation System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox solderNoTextBox;
+        private System.Windows.Forms.TextBox solderNameTextBox;
+        private System.Windows.Forms.TextBox scoreOneTextBox;
+        private System.Windows.Forms.TextBox scoreTwoTextBox;
+        private System.Windows.Forms.TextBox scoreThreeTextBox;
+        private System.Windows.Forms.TextBox scoreFourTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RadioButton solderNoRadioButton;
+        private System.Windows.Forms.RadioButton solderNameRadioButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.ListView infoListView;
+        private System.Windows.Forms.TextBox topAveargeScorerNameTextBox;
+        private System.Windows.Forms.TextBox topTotalScorerTextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ FireRangeAutomation System/FireRangeAutomation System/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace FireRangeAutomation_System
+{     
+    public partial class Form1 : Form
+    {
+        List<ListViewItem> showItem = new List<ListViewItem>();
+        List<string> searchNameItem = new List<string>();
+        List<string> searchNoItem = new List<string>();
+        String topAverageScorerName;
+        string topTotalScorerName;
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+          
+        public void saveButton_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = new ListViewItem();
+            List<float> avg = new List<float>();
+            List<float> total = new List<float>();
+            String solderNo = solderNoTextBox.Text;
+            String solderName= solderNameTextBox.Text;
+            float scoreOne = float.Parse(scoreOneTextBox.Text);
+            float scoreTwo = float.Parse(scoreTwoTextBox.Text);
+            float scoreThree = float.Parse(scoreThreeTextBox.Text);
+            float scoreFour = float.Parse(scoreFourTextBox.Text);
+
+            float totalCount = scoreOne + scoreTwo + scoreThree + scoreFour;
+            float average = (scoreOne + scoreTwo + scoreThree + scoreFour)/4;
+            avg.Add(average);
+            total.Add(totalCount);
+            float temp;
+            for (int j = 0; j < avg.Count; j++)
+            {
+                for (int i = 0; i < avg.Count - 1; i++)
+                {
+                    if (avg[i] < avg[i + 1])
+                    {
+                        temp = avg[i + 1];
+                        avg[i + 1] = avg[i];
+                        avg[i] = temp;
+                    }
+                }
+            }
+            if (avg[0] == average)
+            {
+                topAverageScorerName = solderName;
+            }
+            float totalTemp;
+            for (int j = 0; j < avg.Count; j++)
+            {
+                for (int i = 0; i < avg.Count - 1; i++)
+                {
+                    if (total[i] < total[i + 1])
+                    {
+                        totalTemp = total[i + 1];
+                        total[i + 1] = total[i];
+                        total[i] = totalTemp;
+                    }
+                }
+            }
+            if (total[0] ==totalCount )
+            {
+                topTotalScorerName = solderName;
+            }
+            searchNameItem.Add(solderName);
+            searchNoItem.Add(solderNo);
+            item.Text=solderNo;
+            item.SubItems.Add(solderName);
+            item.SubItems.Add(average.ToString());
+            item.SubItems.Add(totalCount.ToString());
+         
+            showItem.Add(item);
+
+          
+
+            //Clear TextBOx
+            solderNoTextBox.Clear();
+            solderNameTextBox.Clear();
+            scoreOneTextBox.Clear();
+            scoreTwoTextBox.Clear();
+            scoreThreeTextBox.Clear();
+            scoreFourTextBox.Clear();
+
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+
+        
+            for (int i = 0; i < showItem.Count; i++)
+            {
+                infoListView.Items.Add(showItem[i]);
+               
+            }
+
+           
+            topAveargeScorerNameTextBox.Text =  topAverageScorerName;
+            topTotalScorerTextBox.Text = topTotalScorerName;
+        }
+
+
+        private void solderNameRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void solderNoRadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (solderNameRadioButton.Checked)
+            {
+                string searchName = searchTextBox.Text;
+                for (int i = 0; i < searchNameItem.Count; i++)
+                {
+                    infoListView.Items.Clear();
+                    if (searchName == searchNameItem[i])
+                    {
+
+                        infoListView.Items.Add(showItem[i]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Soldier Not Found");
+                        infoListView.Items.Clear();
+                    }
+                }
+                searchTextBox.Clear();
+            }
+
+            if (solderNoRadioButton.Checked)
+            {
+                string searchName = searchTextBox.Text;
+                for (int i = 0; i < searchNoItem.Count; i++)
+                {
+                    infoListView.Items.Clear();
+                    if (searchName == searchNoItem[i])
+                    {
+
+                        infoListView.Items.Add(showItem[i]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Soldier Not Found");
+                        infoListView.Items.Clear();
+                    }
+                }
+                searchTextBox.Clear();
+            }
+
+        }
+    }
+}

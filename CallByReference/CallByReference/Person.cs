174614674f0c6d8a479e070bcca9b7bfa959290a
monirulislam14 <49609116+monirulislam14@users.noreply.github.com>
2019-10-12 23:17:30 +0600

    Call By Reference Example
    Here use Person class to reference and this operator also use here to call constractor as chain.

---
+++ CallByReference/CallByReference/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallByReference
+{
+    class Person
+    {
+        public string firstName;
+        public string midleName;
+        public string lastName;
+        public string id;
+
+        public Person()
+        {
+                 id = "0000";
+          
+        }
+        public Person(string firstName,string lastName):this()
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public Person(string firstName, string midleName,string lastName):this(firstName,lastName)
+        {
+            this.midleName = midleName;
+        }
+        public string GetFullName()
+        {
+            string fullname;
+            if (midleName == null)
+            {
+               fullname =firstName +" " + lastName;
+            }
+            else if (midleName != null)
+            {
+                fullname = " " + firstName + " " + midleName + " " + lastName;
+            }
+            fullname = " " + firstName + " " + midleName + " " + lastName;
+            return fullname;
+        }
+        public string GetReversedName()
+        {
+            string fullName = GetFullName();
+            char[] charArray = fullName.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+    }
+}
---
+++ CallByReference/CallByReference/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallByReference
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string firstName;
+            string midleName;
+            string lastName;
+            Person person1 = new Person("monirul","islam");
+            Person person2 = new Person();
+            //person1 is reference and Person() is object
+           
+            person1.firstName = "monirul";
+            person1.midleName = "islam";
+            person1.lastName = "mondal";
+            person2 = person1;
+          
+           
+           
+            Console.WriteLine(person1.GetFullName());
+            change(person2);
+            Console.WriteLine(person2.GetFullName());
+            Console.WriteLine(person2.GetReversedName());
+            Console.ReadKey();
+        }
+        public static void change(Person person)
+        {
+            person.lastName = "Changed";
+        }
+    }
+}
